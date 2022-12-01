c1ac8cb5c86375dffeaa097c19338f2e33482f54
Doh1024 <dohyeongkim001024@gmail.com>
2022-12-01 13:20:46 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./pages/**/*.vue",
+    "./app.vue",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
