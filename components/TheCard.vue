6e7d2ef4786641c958921cd92c23b54b0354b8cf
Doh1024 <dohyeongkim001024@gmail.com>
2022-12-09 10:36:48 -0600

    image fix for cards

--- components/TheCard.vue
+++ components/TheCard.vue
@@ -4,26 +4,19 @@
       type: String,
       default: "Title",
     },
-    image: {
-      type: Object,
-      default(){
-        return{
-          src: "product name",
-          alt: "img info",
-        };
-      },
-    },
     Instruction:{
       type: String,
       default: "Instruction",
-    }
+    },
+    src: String,
+    alt: String
   });
 </script>
 
 <template>
   <div class="p-20 w-full">
     <div class="bg-white rounded-lg shadow-lg">
-      <img :src="image.scr" :alt="image.alt" class="rounded-t-lg">
+      <img :src="src" :alt="alt" class="rounded-t-lg"/>
       <div class="p-6">
         <h2 class="font-bold mb-2 text-2xl text-purple-800">{{title}}</h2>
         <p class="text-purple-700 mb-2">{{Instruction}}</p>
--- pages/recipes.vue
+++ pages/recipes.vue
@@ -1,12 +1,13 @@
 <script setup>
-  const { data: meals } = await useFetch('www.themealdb.com/api/json/v1/1/random.php')
+  const { data: meals } = await useFetch('https://www.themealdb.com/api/json/v1/1/random.php')
 </script>
 
 <template>
   <div class="grid grid-cols-3">
-    <TheCard v-for="meal in meals"
+    <TheCard v-for="meal in meals.meals"
     :title="meal.strMeal"
-    :image="meal.strMealThumb"
+    :src="meal.strMealThumb"
+    :alt="meal.strMeal"
     :Instruction="meal.strInstructions"/>
   </div>
 </template>
